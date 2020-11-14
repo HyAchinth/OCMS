de6d8c559c02c4c613e32102042ce5ccc1843166
Achinthya <achinth.s@gmail.com>
2020-11-14 21:12:26 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 const express = require("express");
+const app = express();
+
+const port = 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    //res.send('Hello World!')
+    res.json({ "ok": "not ok" })
+})
+
+app.post('/:id', (req, res) => {
+    const data = req.body
+    const num = req.params.id
+    console.log(data)
+    console.log(num)
+    return res.json({"message":"recieved"})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
