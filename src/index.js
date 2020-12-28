61ce3a850bf5c4aebe427b822d1994ba5998f9fb
dragonfist453 <thermosphere453@gmail.com>
2020-12-28 19:10:57 +0530

    Port change from 3000 to 5000

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const { mysql } = require('./db/mysql');
 
-const port = 3000;
+const port = 5000;
 app.use(express.json());
 app.use('/student', require('./routes/student'));
 app.use('/teacher', require('./routes/teacher'));
