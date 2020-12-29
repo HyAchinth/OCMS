e71496944cb88f2682ae75f547e6bf340bacc0dd
dragonfist453 <thermosphere453@gmail.com>
2020-12-29 14:00:51 +0530

    smol get operations to sections

--- src/routes/section.js
+++ src/routes/section.js
@@ -3,6 +3,47 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { mysql } = require('../db/mysql');
 
+// Get all sections
+
+router.get('/', async(req, res) => {
+    try{
+        const [
+            results
+        ] = await mysql.query(
+            'SELECT * from timetable'
+        );
+
+        const sectionString = JSON.stringify(results);
+        const sections = JSON.parse(sectionString)
+        res.json({ok: true, sections});
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+// Get one section
+
+router.get('/:sectionid', async(req, res) => {
+    try{
+        const data = req.params;
+
+        const [
+            results
+        ] = await mysql.query(
+            'SELECT * from timetable where sectionid = (?)',
+            [data.sectionid] 
+        );
+
+        const sectionString = JSON.stringify(results[0]);
+        const sections = JSON.parse(sectionString)
+        res.json({ok: true, sections});
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
 // Get all sections/timetables by deptid
 
 router.get('/dept/:deptid', async(req, res) => {
