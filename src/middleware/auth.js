5022dab5c1e9d17d1a38051f5ed2a86c0e568c06
Achinthya <achinth.s@gmail.com>
2020-12-31 19:30:45 +0530

    middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,26 @@
-const jwt = require("jsonwebtoken");
-const express = require("express");
+const jwt = require('jsonwebtoken');
+const express = require('express');
 /**
- * 
- * @param {express.Request} req 
- * @param {*} res 
- * @param {*} next 
+ *
+ * @param {express.Request} req
+ * @param {*} res
+ * @param {*} next
  */
-const auth = (role)=>async (req,res,next)=>{
+const auth = role => async (req, res, next) => {
     try {
-        const token = req.header("x-auth-token");
-        jwt.verify(token,process.env.ACCESS_TOKEN,(error,decoded)=>{
-            if(error) throw error;
-            req.user=decoded.user;
-            req.token=token;
-            if(req.user.userType!=role){
-                throw new Error("Wrong Role!")
+        const token = req.header('x-auth-token');
+        jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+            if (error) throw error;
+            req.user = decoded.user;
+            req.token = token;
+            if (req.user.userType != role) {
+                throw new Error('Wrong Role!');
             }
 
             next();
         });
     } catch (error) {
-        res.status(401).json({error:"Invalid Token!"});
+        res.status(401).json({ error: 'Invalid Token!' });
     }
 };
 
